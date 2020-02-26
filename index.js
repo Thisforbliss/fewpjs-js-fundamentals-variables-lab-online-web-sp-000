8a6b11df977a3780b2cf2f848db929d3ee65c616
Malcolm Leake <echelon1089@gmail.com>
2020-02-26 23:20:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 // Code your solution in this file!
-const companyName = 'Scuber';
+const companyName = 'Scuber';https://learn.co/tracks/full-stack-web-development-v8/module-14-front-end-web-programming-in-javascript/section-2-manipulating-the-dom/js-fundamentals-variables-lab#
